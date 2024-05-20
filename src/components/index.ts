f408c91e52368447a454203343089960dc74c883
ChuckmonsterXlx <alfonso_309@hotmail.com>
2024-05-20 16:42:21 -0700

    sidebar was added

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+// sidebar
+export { Sidebar } from "./sidebar/Siderbar";
+
+// menu
+export { TopMenu } from "./menu/top-menu/TopMenu";
+
+// widget
+export { WidgetItem } from "./widget/widget-item/WidgetItem";
